bb07d358427feb234511218fb662c2d6d4bc37cb
Francisco Hurtado <u202117498@upc.edu.pe>
2026-03-11 20:14:57 -0500

    feat: Implement a responsive navigation header component with CV download functionality.

--- src/components/NavigationHeader.vue
+++ src/components/NavigationHeader.vue
@@ -13,6 +13,14 @@
           <a href="#projects" class="nav-link" @click="closeMenu">Proyectos</a>
           <a href="#education" class="nav-link" @click="closeMenu">Educación</a>
           <a href="#footer" class="nav-link" @click="closeMenu">Contacto</a>
+          <a href="/CV.pdf" download="CV_Francisco_Hurtado.pdf" class="btn" @click="closeMenu">
+            Descargar CV
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon-download">
+              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+              <polyline points="7 10 12 15 17 10"></polyline>
+              <line x1="12" y1="15" x2="12" y2="3"></line>
+            </svg>
+          </a>
         </div>
 
         <!-- Mobile Menu Toggle -->
